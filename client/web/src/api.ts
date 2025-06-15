317175d194c43b95f49bcc9ae352500dfa04282c
Andrew MacDonald <amacdonald@slack-corp.com>
2025-06-15 13:28:56 -0700

    dummy data

--- client/web/src/api.ts
+++ client/web/src/api.ts
@@ -8,10 +8,21 @@ export async function fetchPuzzle(): Promise<{
     setTimeout(
       () =>
         resolve({
-          grid: Array.from({ length: 10 }, () => Array(10).fill("")),
-          words: [],
+          grid: [
+            ["C", "A", "T", "U", "D", "A", "X", "I", "H", "H"],
+            ["E", "X", "D", "D", "O", "G", "V", "X", "R", "C"],
+            ["S", "B", "I", "R", "D", "N", "B", "A", "C", "G"],
+            ["H", "Q", "T", "A", "R", "G", "F", "W", "U", "W"],
+            ["R", "N", "H", "O", "S", "I", "I", "Z", "A", "Y"],
+            ["Z", "F", "W", "N", "K", "I", "S", "E", "G", "Y"],
+            ["K", "D", "C", "M", "D", "L", "H", "L", "T", "I"],
+            ["Z", "B", "X", "O", "R", "D", "M", "C", "R", "J"],
+            ["U", "T", "L", "S", "G", "W", "C", "B", "V", "H"],
+            ["Y", "J", "C", "H", "D", "M", "I", "O", "U", "L"],
+          ],
+          words: ["CAT", "DOG", "BIRD", "FISH"],
         }),
-      5000
+      1000
     )
   );
 }
